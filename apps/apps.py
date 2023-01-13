7d064c4a418f0d5d19243f95beeb45a27c0b16f7
Anthony Rozario <109983324+anthonyrozario-codefresh@users.noreply.github.com>
2023-01-13 11:19:42 -0800

    adding initial files

---
+++ apps/apps.py
@@ -0,0 +1,9 @@
+from flask import Flask
+application = Flask(__name__)
+
+@application.route("/")
+def index():
+    return "This application was built on Codefresh"
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port='8080')
